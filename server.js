aa75cdf063d7fafa27a75b3fa5dcac59396abbbc
manthan ankolekar <manthan.ank46@gmail.com>
2022-05-20 18:21:22 +0530

    changes made

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
