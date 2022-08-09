3b0c2571dba51b9e9170dc6b72c1a261fea08551
Manthan Ankolekar <manthan.ank46@gmail.com>
2022-08-09 23:09:09 +0530

    make it better

--- server.js
+++ server.js
@@ -42,6 +42,8 @@ app.get("/", (req, res) => {
 require("./routes/auth.routes")(app);
 require("./routes/user.routes")(app);
 
+require('dotenv').config()
+
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
